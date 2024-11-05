ce1a078249433d1d134e49a1b9ab4f31eefac2ad
Cahyono Wahyu Setiawan <cahyonowahyus123@gmail.com>
2024-11-05 11:31:14 +0700

    config: config metro

--- packages/AwesomeTSProject/metro.config.js
+++ packages/AwesomeTSProject/metro.config.js
@@ -1,21 +1,25 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
 const path = require('path');
 
-const watchFolders = [path.resolve(__dirname + '/..')];
+const extraNodeModules = {
+  modules: path.resolve(path.join(__dirname, '../../packages')),
+};
+
+const watchFolders = [path.resolve(path.join(__dirname, '../../packages'))];
+
+const nodeModulesPaths = [path.resolve(path.join(__dirname, './node_modules'))];
 
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
+        experimentalImportSupport: true,
+        inlineRequires: true,
       },
     }),
+  },
+  resolver: {
+    extraNodeModules,
+    nodeModulesPaths,
   },
   watchFolders,
 };
